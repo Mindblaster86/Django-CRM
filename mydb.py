97931856ca16f51711d94e85f530b47dbb5e05c8
Vincent Doyle <vgd8690@gmail.com>
2023-11-21 16:56:31 -0500

    registration

--- mydb.py
+++ mydb.py
@@ -1,22 +1,22 @@
 # Install Mysql on your computer
 # https://dev.mysql.com/downloads/installer/
 # pip install mysql
-# pip*install*mysql-connector
-# pip install mysql-connector-python
+# pip install mysql-connector
+# pip install mysql-connector-python 
 
 import mysql.connector
 
 dataBase = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = 'Cottonwood8690!',
+	host = 'localhost',
+	user = 'root',
+	passwd = 'password123'
 
-    )
+	)
 
 # prepare a cursor object
 cursorObject = dataBase.cursor()
 
-#create a database
-cursorObject.execute("CREATE DATABASE doyles")
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
 
-print("ALL DONE!")
+print("All Done!")
